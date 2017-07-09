b7e01789ee3d705132550ac241e4d9d503449088
zhangyancheng <1392364470@qq.com>
2017-07-09 22:25:57 +0800

    1

--- single_step_run.py
+++ single_step_run.py
@@ -1,52 +1,73 @@
 from selenium import webdriver
+import unittest
 from time import sleep
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import  By
 from selenium.webdriver.common.action_chains import  ActionChains
 from selenium.webdriver.common.keys import Keys
 import os
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(10)
-driver.maximize_window()
-driver.get("http://www.fengsulian.com/user/cards")
+class MyTest(unittest.TestCase):
 
-driver.find_element_by_id("username").send_keys("15868147450")
-driver.find_element_by_id("password").send_keys("12345678")
-driver.find_element_by_css_selector("input[class = 'btn_com btn_blue login_btn_submit sy_in']").click()
+    #定位器1
+    login_username_loc = (By.ID, "username")
+    login_password_loc = (By.ID, "password")
+    login_button_loc = (By.CSS_SELECTOR, "input[class = 'btn_com btn_blue login_btn_submit sy_in']")
+    #定位器2
+    user_login_success_loc = (By.XPATH, "//div[1]/div[1]/div[1]/div/div[1]/div[1]/span")
 
-# ele0 = driver.find_element_by_css_selector("a[class='per_active']").text    #定位器2【进入名片管理页面】
-# print(ele0)
-#
-# driver.find_element_by_css_selector("a[class='ss_nearcom']").click()        #定位器1【点击查看企业】
-# ele1 = driver.find_element_by_css_selector("a[href='/enterprise/1549']>h2").text     #定位器2【查看企业】
-# print(ele1)
-#
-# driver.find_element_by_css_selector("div[class='wf_content_list_view fr']>a").click()   #定位器1【点击查看名片】
-# ele2 = driver.find_element_by_css_selector("div[class='title cardName_ajax']").text     #定位器2【点击查看名片】
-# print(ele2)
-#
-# driver.find_element_by_css_selector("i[class='fa fa-heart-o']").click()          #定位器1【点击收藏】
-# ele3 = driver.find_element_by_css_selector("span[class='collect']").text        #定位器2【收藏成功】
-# print(ele3)
+    @classmethod
+    def setUpClass(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get("http://www.fengsulian.com/login")
 
-driver.get("http://www.fengsulian.com/user/cards")
-ele4 = driver.find_element_by_css_selector("p[class='p_text']").text             #定位器2【名片需求里收藏成功】
-print(ele4)
+    def element_wait(self,css, secs=8):
+        """
+        显示等待
+        """
+        by = css[0].strip()
+        messages = 'Element: {0} not found in {1} seconds.'.format(css, secs)
+        if by == "id":
+            WebDriverWait(self.driver, secs, 0.5).until(EC.presence_of_element_located(css), messages)
+        elif by == "name":
+            WebDriverWait(self.driver, secs, 0.5).until(EC.presence_of_element_located(css), messages)
+        elif by == "class name":
+            WebDriverWait(self.driver, secs, 0.5).until(EC.presence_of_element_located(css), messages)
+        elif by == "link text":
+            WebDriverWait(self.driver, secs, 0.5).until(EC.presence_of_element_located(css), messages)
+        elif by == "xpath":
+            WebDriverWait(self.driver, secs, 0.5).until(EC.presence_of_element_located(css), messages)
+        elif by == "css selector":
+            WebDriverWait(self.driver, secs, 0.5).until(EC.presence_of_element_located(css), messages)
+        else:
+            raise NameError("Please enter the correct targeting elements,'id','name','class','link_text','xpath','css'.")
 
-# driver.find_element_by_css_selector("a[class='see_card']").click()               #定位器1【需求列表点击查看名片】
-# print(ele3)
+    def login_username(self):
+        """要传入多个值，用位置参数常用于元组"""
+        self.element_wait(css=self.login_username_loc)
+        self.driver.find_element(*self.login_username_loc).clear()
+        self.driver.find_element(*self.login_username_loc).send_keys("15868147450")
+    def login_password(self):
+        self.element_wait(css=self.login_password_loc)
+        self.driver.find_element(*self.login_password_loc).clear()
+        self.driver.find_element(*self.login_password_loc).send_keys("12345678")
+    def login_button(self):
+        self.element_wait(css=self.login_button_loc)
+        self.driver.find_element(*self.login_button_loc).click()
 
-driver.find_element_by_css_selector("div[class='del_btn']>i").click()            #定位器1【点击删除收藏】
-ele5 = driver.find_element_by_css_selector("div[class='dialog_main dialog_alert']").text  #定位器2【弹出删除收藏框】
-print(ele5)
+    def test_login1(self):
+        self.login_username()
+        self.login_password()
+        self.login_button()
+        self.element_wait(css=self.user_login_success_loc)
+        expected = self.driver.find_element(*self.user_login_success_loc).text
+        self.assertEqual(expected,"FSL_62223599   ")
 
-driver.find_element_by_css_selector("div[class='cancel btn_com btn_red ml30']") .click()   #定位器1【取消删除收藏】
-print(ele4)
+    @classmethod
+    def tearDownClass(self):
+        self.driver.quit()
 
-driver.find_element_by_css_selector("div[class='del_btn']>i").click()
-sleep(1)
-driver.find_element_by_css_selector("div[class='submit btn_com btn_blue']").click()        #定位器1【确定删除收藏】
-sleep(1)
-ele6 = driver.find_element_by_css_selector("a[class='ss_nearcom']").text                    #定位器2【确定删除收藏】
-print(ele6)
-sleep(3)
-driver.quit()
+if __name__ == "__mian__":
+    unittest.main()
