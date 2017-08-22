313a2c76555dc976f9b8b3a2a29130dedac751c6
zhangyancheng <1392364470@qq.com>
2017-08-22 17:25:18 +0800

    1

--- test_case/page_obj/a7_pay_password.py
+++ test_case/page_obj/a7_pay_password.py
@@ -21,13 +21,13 @@ class paypwd(Log_on,Page):
         self.click(self.button_pay_loc)
     def input_paypwd(self,pay):
         """设置交易密码"""
-        self.clear_type(self.input_paypwd_loc,pay)
+        self.clear_type(self.input_paypwd_loc, pay)
     def confirm_paypwd(self,ppay):
         """确认交易密码"""
-        self.clear_type(self.confirm_paypwd_loc,ppay)
+        self.clear_type(self.confirm_paypwd_loc, ppay)
     def iden_code(self,code):
         """输入验证码"""
-        self.clear_type(self.identifying_code_loc,code)
+        self.clear_type(self.identifying_code_loc, code)
     def confirm_modify(self):
         """确认修改交易密码"""
         self.click(self.confirm_modify_loc)
@@ -55,15 +55,15 @@ class paypwd(Log_on,Page):
     #action
     def enter_paypage(self,actual):
         """进入交易密码页面"""
-        return self.is_text_in_element(self.enter_paypage_loc,actual)
+        return self.is_text_in_element(self.enter_paypage_loc, actual)
     def paypwd_error(self,actual):
         """设置交易密码错误"""
-        return self.is_text_in_element(self.paypwd_error_loc,actual)
+        return self.is_text_in_element(self.paypwd_error_loc, actual)
     def ppaypwd_error(self,actual):
         """确认交易密码错误"""
-        return self.is_text_in_element(self.ppypwd_error_loc,actual)
+        return self.is_text_in_element(self.ppypwd_error_loc, actual)
     def code_error(self,actual):
         """验证码错误"""
-        return self.is_text_in_element(self.code_error_loc,actual)
+        return self.is_text_in_element(self.code_error_loc, actual)
 
 
